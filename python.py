9c59f79b8646c545af7645cc1847987be72e9ec9
shoplinewilliam <william@shoplineapp.com>
2017-11-19 23:35:59 +0800

    trying to print from terminal but fail

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
 print "hello world"
+print "Hello Again"
+print "I like typing this."
+print "this is fun"
+print 'yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not thouch this.'
